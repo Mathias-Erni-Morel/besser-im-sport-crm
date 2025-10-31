71683fcbc999d238e29b418a2537575553536167
Mathias-Erni-Morel <erni.massagen@gmail.com>
2025-10-31 10:53:02 +0100

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,9 +1,8 @@
-
 'use client';
 
 import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
 
 export const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
